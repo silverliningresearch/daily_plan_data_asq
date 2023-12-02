56b368165df89627e87ea04598c6a43a90cc559a
unknown <lb@silverliningresearch.com>
2023-12-02 19:59:17 +0000

    update new data

--- MUC/Departures/interview_statistics_asq.js
+++ MUC/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2023-07-03",
         "quota_id": "LH-AGP",
         "Number of interviews": 3,
-        "download_time": "02-12-2023 14:49:47"
+        "download_time": "02-12-2023 20:58:54"
     },
     {
         "InterviewDate": "2023-07-03",
