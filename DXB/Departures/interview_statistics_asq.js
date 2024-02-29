dc15f59d250124770ef1f75930d50950561a4576
unknown <lb@silverliningresearch.com>
2024-02-29 20:45:12 +0000

    update new data

--- DXB/Departures/interview_statistics_asq.js
+++ DXB/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2023-10-06",
         "quota_id": "EK-BKK",
         "Number of interviews": 10,
-        "download_time": "29-02-2024 22:41:23"
+        "download_time": "01-03-2024 00:45:09"
     },
     {
         "InterviewDate": "2023-10-06",
