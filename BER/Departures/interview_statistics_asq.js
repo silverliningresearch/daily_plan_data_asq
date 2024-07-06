57452f7a53e7cd2fea86b5efb6f3d7bb72a2899e
unknown <lb@silverliningresearch.com>
2024-07-06 11:22:14 +0000

    update new data

--- BER/Departures/interview_statistics_asq.js
+++ BER/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-01-08",
         "quota_id": "BA-LHR",
         "Number of interviews": 3,
-        "download_time": "06-07-2024 11:13:20"
+        "download_time": "06-07-2024 13:22:13"
     },
     {
         "InterviewDate": "2024-01-08",
@@ -2683,6 +2683,21 @@ let interview_statistics_asq = `[
     {
         "InterviewDate": "2024-07-06",
         "quota_id": "A3-ATH",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-07-06",
+        "quota_id": "DS-BSL",
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-07-06",
+        "quota_id": "EC-BASEL",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-07-06",
+        "quota_id": "EC-LIN",
         "Number of interviews": 4
     },
     {
--- VIE/Departures/interview_statistics_asq.js
+++ VIE/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-01-06",
         "quota_id": "BA-LHR",
         "Number of interviews": 4,
-        "download_time": "06-07-2024 11:13:05"
+        "download_time": "06-07-2024 13:21:58"
     },
     {
         "InterviewDate": "2024-01-06",
@@ -4692,7 +4692,27 @@ let interview_statistics_asq = `[
     },
     {
         "InterviewDate": "2024-07-06",
+        "quota_id": "OS-ATH",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-07-06",
         "quota_id": "OS-FRA",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-07-06",
+        "quota_id": "OS-MUC",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-07-06",
+        "quota_id": "OS-MXP",
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-07-06",
+        "quota_id": "OS-SPU",
+        "Number of interviews": 6
     }
 ] `;
