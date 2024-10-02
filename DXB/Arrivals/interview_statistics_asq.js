dbde64e067fe56fc7d75844553b53f42b91bd391
unknown <lb@silverliningresearch.com>
2024-10-02 20:16:29 +0000

    update new data

--- DXB/Arrivals/interview_statistics_asq.js
+++ DXB/Arrivals/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "01/02/2024",
         "quota_id": "EK-COK",
         "Number of interviews": 2,
-        "download_time": "02-10-2024 23:21:35"
+        "download_time": "03-10-2024 00:16:21"
     },
     {
         "InterviewDate": "01/02/2024",
--- JED/Departures/statistics.js
+++ JED/Departures/statistics.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2023-10-02",
         "quota_id": "F3-AHB",
         "Number of interviews": 1,
-        "download_time": "02-10-2024 22:21:42"
+        "download_time": "02-10-2024 23:16:29"
     },
     {
         "InterviewDate": "2023-10-02",
@@ -3707,6 +3707,11 @@ let interview_statistics_asq = `[
     },
     {
         "InterviewDate": "2024-10-02",
+        "quota_id": "F3-DMM",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-02",
         "quota_id": "F3-RUH",
         "Number of interviews": 4
     },
@@ -3749,5 +3754,15 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-10-02",
         "quota_id": "XY-DMM",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-10-02",
+        "quota_id": "XY-RUH",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "SV-TUU",
+        "Number of interviews": 1
     }
 ] `;
