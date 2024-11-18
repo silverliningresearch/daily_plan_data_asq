a0966a8fdf0d8b65b0d9f18a40b74e3685836dc4
unknown <lb@silverliningresearch.com>
2024-11-18 11:23:33 +0000

    update new data

--- BER/Departures/interview_statistics_asq.js
+++ BER/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-01-08",
         "quota_id": "BA-LHR",
         "Number of interviews": 3,
-        "download_time": "18-11-2024 11:12:22"
+        "download_time": "18-11-2024 12:23:30"
     },
     {
         "InterviewDate": "2024-01-08",
@@ -4863,7 +4863,7 @@ let interview_statistics_asq = `[
     {
         "InterviewDate": "2024-11-18",
         "quota_id": "BA-LCY",
-        "Number of interviews": 6
+        "Number of interviews": 11
     },
     {
         "InterviewDate": "2024-11-18",
@@ -4884,5 +4884,15 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-11-18",
         "quota_id": "EC-VLC",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-11-18",
+        "quota_id": "EW-CGN",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-18",
+        "quota_id": "TP-LIS",
+        "Number of interviews": 5
     }
 ] `;
--- VIE/Departures/interview_statistics_asq.js
+++ VIE/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-07-02",
         "quota_id": "KL-AMS",
         "Number of interviews": 8,
-        "download_time": "18-11-2024 11:11:47"
+        "download_time": "18-11-2024 12:22:55"
     },
     {
         "InterviewDate": "2024-07-04",
@@ -3577,7 +3577,17 @@ let interview_statistics_asq = `[
     },
     {
         "InterviewDate": "2024-11-18",
+        "quota_id": "LH-MUC",
+        "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-11-18",
         "quota_id": "OS-HAJ",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-11-18",
+        "quota_id": "OS-VAR",
+        "Number of interviews": 5
     }
 ] `;
