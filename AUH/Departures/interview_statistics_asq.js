11207588be51d306c9802297dcd8e0cfc8151f2a
unknown <lb@silverliningresearch.com>
2024-12-08 08:05:48 +0000

    update new data

--- AUH/Departures/interview_statistics_asq.js
+++ AUH/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "EY-BOM",
         "Number of interviews": 5,
-        "download_time": "08-12-2024 10:54:26"
+        "download_time": "08-12-2024 12:05:46"
     },
     {
         "InterviewDate": "2024-10-02",
@@ -763,6 +763,11 @@ let interview_statistics_asq = `[
     {
         "InterviewDate": "2024-12-08",
         "quota_id": "3L-KTM",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-12-08",
+        "quota_id": "3L-TBS",
         "Number of interviews": 1
     },
     {
--- VIE/Departures/interview_statistics_asq.js
+++ VIE/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-07-02",
         "quota_id": "KL-AMS",
         "Number of interviews": 8,
-        "download_time": "08-12-2024 07:53:44"
+        "download_time": "08-12-2024 09:05:03"
     },
     {
         "InterviewDate": "2024-07-04",
