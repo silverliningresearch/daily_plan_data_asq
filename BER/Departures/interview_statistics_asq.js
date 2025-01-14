e3e7569a888528daca0794e134985ac84cbc06c0
unknown <lb@silverliningresearch.com>
2025-01-14 10:53:10 +0000

    update new data

--- BER/Departures/interview_statistics_asq.js
+++ BER/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-01-08",
         "quota_id": "BA-LHR",
         "Number of interviews": 3,
-        "download_time": "14-01-2025 10:42:15"
+        "download_time": "14-01-2025 11:53:02"
     },
     {
         "InterviewDate": "2024-01-08",
--- MUC/Departures/interview_statistics_asq.js
+++ MUC/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2023-01-05",
         "quota_id": "BA-LHR",
         "Number of interviews": 6,
-        "download_time": "14-01-2025 10:41:43"
+        "download_time": "14-01-2025 11:52:31"
     },
     {
         "InterviewDate": "2023-01-05",
@@ -8173,12 +8173,22 @@ let interview_statistics_asq = `[
     {
         "InterviewDate": "2025-01-14",
         "quota_id": "AF-CDG",
-        "Number of interviews": 1
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2025-01-14",
         "quota_id": "AZ-FCO",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2025-01-14",
+        "quota_id": "BA-LHR",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2025-01-14",
+        "quota_id": "TK-IST",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2025-01-14",
