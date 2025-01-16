4ca255214d772432686975296a75fd1d96487f31
unknown <lb@silverliningresearch.com>
2025-01-16 08:07:08 +0000

    update new data

--- VIE/Departures/interview_statistics_asq.js
+++ VIE/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-07-02",
         "quota_id": "KL-AMS",
         "Number of interviews": 8,
-        "download_time": "16-01-2025 07:56:12"
+        "download_time": "16-01-2025 09:06:37"
     },
     {
         "InterviewDate": "2024-07-04",
