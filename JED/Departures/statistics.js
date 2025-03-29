4d44f2d5c21717490acfffcf25ef46de97bf7e85
unknown <lb@silverliningresearch.com>
2025-03-29 18:35:05 +0000

    update new data

--- JED/Departures/statistics.js
+++ JED/Departures/statistics.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2023-10-02",
         "quota_id": "F3-AHB",
         "Number of interviews": 1,
-        "download_time": "29-03-2025 20:23:55"
+        "download_time": "29-03-2025 21:35:00"
     },
     {
         "InterviewDate": "2023-10-02",
