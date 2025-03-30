279b73a74760e206ae82ca9fa8ea8c142e4b3954
unknown <lb@silverliningresearch.com>
2025-03-30 03:55:22 +0000

    update new data

--- DXB/Arrivals/interview_statistics_asq.js
+++ DXB/Arrivals/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "01/01/2025",
         "quota_id": "0-TLV",
         "Number of interviews": 2,
-        "download_time": "30-03-2025 06:43:53"
+        "download_time": "30-03-2025 07:55:05"
     },
     {
         "InterviewDate": "01/01/2025",
--- VIE/Departures/interview_statistics_asq.js
+++ VIE/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-07-02",
         "quota_id": "KL-AMS",
         "Number of interviews": 8,
-        "download_time": "30-03-2025 04:43:20"
+        "download_time": "30-03-2025 05:54:34"
     },
     {
         "InterviewDate": "2024-07-04",
