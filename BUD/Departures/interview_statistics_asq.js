4dd073ab2137abee036a81d1278257c08569249a
unknown <lb@silverliningresearch.com>
2025-04-18 07:42:17 +0000

    update new data

--- BUD/Departures/interview_statistics_asq.js
+++ BUD/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-01-02",
         "quota_id": "FR-BCN",
         "Number of interviews": 4,
-        "download_time": "18-04-2025 08:30:14"
+        "download_time": "18-04-2025 09:41:57"
     },
     {
         "InterviewDate": "2024-01-02",
