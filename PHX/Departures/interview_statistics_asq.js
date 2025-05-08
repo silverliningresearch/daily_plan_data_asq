599cdd42421dc6b792ce9d9e628239af2aef0a0b
unknown <lb@silverliningresearch.com>
2025-05-08 03:26:13 +0000

    update new data

--- PHX/Departures/interview_statistics_asq.js
+++ PHX/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2025-01-09",
         "quota_id": "AA-MEX",
         "Number of interviews": 6,
-        "download_time": "08-05-2025 06:21:52"
+        "download_time": "08-05-2025 07:26:13"
     },
     {
         "InterviewDate": "2025-01-09",
