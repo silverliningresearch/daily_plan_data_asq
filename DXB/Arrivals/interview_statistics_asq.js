8e6f02381762d8ad4e8315d848e2446c2864c7a9
unknown <lb@silverliningresearch.com>
2025-05-17 07:56:44 +0000

    update new data

--- DXB/Arrivals/interview_statistics_asq.js
+++ DXB/Arrivals/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "01/01/2025",
         "quota_id": "0-TLV",
         "Number of interviews": 2,
-        "download_time": "17-05-2025 10:43:59"
+        "download_time": "17-05-2025 11:56:17"
     },
     {
         "InterviewDate": "01/01/2025",
