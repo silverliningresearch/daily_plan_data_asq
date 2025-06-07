726141081c4c51db15b70cbfafc391d040a970f7
unknown <lb@silverliningresearch.com>
2025-06-07 06:21:21 +0000

    update new data

--- VIE/Departures/interview_statistics_asq.js
+++ VIE/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-07-02",
         "quota_id": "KL-AMS",
         "Number of interviews": 8,
-        "download_time": "07-06-2025 07:08:23"
+        "download_time": "07-06-2025 08:20:28"
     },
     {
         "InterviewDate": "2024-07-04",
@@ -7709,5 +7709,10 @@ let interview_statistics_asq = `[
         "InterviewDate": "2025-06-07",
         "quota_id": "OS-DUS",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2025-06-07",
+        "quota_id": "OS-FRA",
+        "Number of interviews": 4
     }
 ] `;
