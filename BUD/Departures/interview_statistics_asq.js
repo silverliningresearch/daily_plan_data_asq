acaea4df9477a75249b5d8f1301533604ce98d2e
unknown <lb@silverliningresearch.com>
2025-07-05 04:15:44 +0000

    update new data

--- BUD/Departures/interview_statistics_asq.js
+++ BUD/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2025-04-02",
         "quota_id": "EC-CDG",
         "Number of interviews": 1,
-        "download_time": "05-07-2025 05:04:17"
+        "download_time": "05-07-2025 06:15:24"
     },
     {
         "InterviewDate": "2025-04-02",
--- DXB/Arrivals/interview_statistics_asq.js
+++ DXB/Arrivals/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "01/01/2025",
         "quota_id": "0-TLV",
         "Number of interviews": 2,
-        "download_time": "05-07-2025 07:04:16"
+        "download_time": "05-07-2025 08:15:22"
     },
     {
         "InterviewDate": "01/01/2025",
@@ -2997,13 +2997,33 @@ let interview_statistics_asq = `[
     },
     {
         "InterviewDate": "05/07/2025",
+        "quota_id": "EK-JFK",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "05/07/2025",
+        "quota_id": "EK-LGW",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "05/07/2025",
         "quota_id": "EK-LHR",
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "05/07/2025",
+        "quota_id": "EK-MAD",
         "Number of interviews": 2
     },
     {
         "InterviewDate": "05/07/2025",
         "quota_id": "EK-MLE",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "05/07/2025",
+        "quota_id": "EK-STN",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "05/10/2024",
