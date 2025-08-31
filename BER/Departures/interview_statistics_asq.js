dc0b27e1307561ef379eea6539c3272f0fc5e912
unknown <lb@silverliningresearch.com>
2025-08-31 05:19:08 +0000

    update new data

--- BER/Departures/interview_statistics_asq.js
+++ BER/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-01-08",
         "quota_id": "BA-LHR",
         "Number of interviews": 3,
-        "download_time": "31-08-2025 06:07:04"
+        "download_time": "31-08-2025 07:18:51"
     },
     {
         "InterviewDate": "2024-01-08",
--- BUD/Departures/interview_statistics_asq.js
+++ BUD/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2025-04-02",
         "quota_id": "EC-CDG",
         "Number of interviews": 1,
-        "download_time": "31-08-2025 06:07:00"
+        "download_time": "31-08-2025 07:18:45"
     },
     {
         "InterviewDate": "2025-04-02",
