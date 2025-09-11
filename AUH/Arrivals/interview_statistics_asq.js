d8ddc26870f6662acff63d7a1c1a90322b7316b0
unknown <lb@silverliningresearch.com>
2025-09-11 12:09:33 +0000

    update new data

--- AUH/Arrivals/interview_statistics_asq.js
+++ AUH/Arrivals/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "01/01/2025",
         "quota_id": "EY-JED",
         "Number of interviews": 1,
-        "download_time": "11-09-2025 15:04:26"
+        "download_time": "11-09-2025 16:09:24"
     },
     {
         "InterviewDate": "01/01/2025",
