86785513885ba6d30129a01e128964447300aa4d
unknown <lb@silverliningresearch.com>
2025-10-23 06:14:19 +0000

    update new data

--- AUH/Departures/interview_statistics_asq.js
+++ AUH/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "EY-BOM",
         "Number of interviews": 5,
-        "download_time": "23-10-2025 09:02:41"
+        "download_time": "23-10-2025 10:14:13"
     },
     {
         "InterviewDate": "2024-10-02",
