fc1c318644c423998717411573cfb0a7cb8744b4
unknown <lb@silverliningresearch.com>
2026-01-06 07:26:54 +0000

    update new data

---
+++ DUS/Departures/interview_statistics_asq.js
@@ -0,0 +1,8 @@
+let interview_statistics_asq = `[
+    {
+        "InterviewDate": "2025-04-02",
+        "quota_id": "99-999",
+        "Number of interviews": 0,
+        "download_time": "06-01-2026 07:55:13"
+    }
+] `;
