7793c737521548ac2441d0c3a77b6e85577be7a7
unknown <lb@silverliningresearch.com>
2026-03-13 16:07:26 +0000

    update new data

--- AUH/Arrivals/interview_statistics_asq.js
+++ AUH/Arrivals/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "01/01/2025",
         "quota_id": "EY-JED",
         "Number of interviews": 1,
-        "download_time": "13-03-2026 18:54:41"
+        "download_time": "13-03-2026 20:07:18"
     },
     {
         "InterviewDate": "01/01/2025",
--- BER/Departures/interview_statistics_asq.js
+++ BER/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-01-08",
         "quota_id": "BA-LHR",
         "Number of interviews": 3,
-        "download_time": "13-03-2026 15:54:26"
+        "download_time": "13-03-2026 17:07:03"
     },
     {
         "InterviewDate": "2024-01-08",
@@ -10492,7 +10492,27 @@ let interview_statistics_asq = `[
     },
     {
         "InterviewDate": "2026-03-13",
+        "quota_id": "BA-LCY",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2026-03-13",
+        "quota_id": "BA-LHR",
+        "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2026-03-13",
+        "quota_id": "EC-GATWICK",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2026-03-13",
         "quota_id": "KL-AMS",
-        "Number of interviews": 8
+        "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2026-03-13",
+        "quota_id": "VF-ESB",
+        "Number of interviews": 2
     }
 ] `;
