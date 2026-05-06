536a984e0f5255eee865c6c7268109811046ec15
unknown <lb@silverliningresearch.com>
2026-05-06 11:54:00 +0000

    update new data

--- DUS/Departures/interview_statistics_asq.js
+++ DUS/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2025-10-03",
         "quota_id": "DE-ACE",
         "Number of interviews": 1,
-        "download_time": "06-05-2026 12:41:21"
+        "download_time": "06-05-2026 13:53:59"
     },
     {
         "InterviewDate": "2025-10-03",
