591adb86d69c93a3bcc15e206b6971fe042ed700
unknown <lb@silverliningresearch.com>
2026-06-22 14:22:58 +0000

    update new data

--- BUD/Departures/interview_statistics_asq.js
+++ BUD/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2025-04-02",
         "quota_id": "EC-CDG",
         "Number of interviews": 1,
-        "download_time": "22-06-2026 15:18:01"
+        "download_time": "22-06-2026 16:22:26"
     },
     {
         "InterviewDate": "2025-04-02",
