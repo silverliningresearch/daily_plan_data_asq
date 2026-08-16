3fe698a69cd61e82b849b4798675f1de4fba2739
unknown <lb@silverliningresearch.com>
2026-08-16 18:29:15 +0000

    update new data

--- AUH/Departures/interview_statistics_asq.js
+++ AUH/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "EY-BOM",
         "Number of interviews": 5,
-        "download_time": "16-08-2026 21:16:20"
+        "download_time": "16-08-2026 22:29:04"
     },
     {
         "InterviewDate": "2024-10-02",
