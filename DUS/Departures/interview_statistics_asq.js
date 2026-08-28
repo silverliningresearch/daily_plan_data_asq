f628000f36730e6e1844219333860695ac25f7c6
unknown <lb@silverliningresearch.com>
2026-08-28 16:48:53 +0000

    update new data

--- DUS/Departures/interview_statistics_asq.js
+++ DUS/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2025-10-03",
         "quota_id": "DE-ACE",
         "Number of interviews": 1,
-        "download_time": "28-08-2026 17:35:59"
+        "download_time": "28-08-2026 18:48:53"
     },
     {
         "InterviewDate": "2025-10-03",
--- MUC/Departures/interview_statistics_asq.js
+++ MUC/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2023-01-05",
         "quota_id": "BA-LHR",
         "Number of interviews": 6,
-        "download_time": "28-08-2026 17:34:44"
+        "download_time": "28-08-2026 18:47:38"
     },
     {
         "InterviewDate": "2023-01-05",
@@ -16618,6 +16618,16 @@ let interview_statistics_asq = `[
     {
         "InterviewDate": "2026-08-28",
         "quota_id": "LH-CDG",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2026-08-28",
+        "quota_id": "LH-CGN",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2026-08-28",
+        "quota_id": "LX-ZRH",
         "Number of interviews": 2
     }
 ] `;
