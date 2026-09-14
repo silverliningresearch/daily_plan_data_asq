1811c8bb4baa68009c3935604a40dc795b82da30
unknown <lb@silverliningresearch.com>
2026-09-14 20:38:51 +0000

    update new data

--- AUH/Departures/interview_statistics_asq.js
+++ AUH/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "EY-BOM",
         "Number of interviews": 5,
-        "download_time": "14-09-2026 23:25:07"
+        "download_time": "15-09-2026 00:38:38"
     },
     {
         "InterviewDate": "2024-10-02",
--- PHX/Departures/interview_statistics_asq.js
+++ PHX/Departures/interview_statistics_asq.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2025-01-09",
         "quota_id": "AA-MEX",
         "Number of interviews": 6,
-        "download_time": "14-09-2026 23:25:18"
+        "download_time": "15-09-2026 00:38:49"
     },
     {
         "InterviewDate": "2025-01-09",
@@ -11508,36 +11508,96 @@ let interview_statistics_asq = `[
     {
         "InterviewDate": "2026-09-14",
         "quota_id": "AA-BFL",
-        "Number of interviews": 1
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2026-09-14",
         "quota_id": "AA-CLD",
-        "Number of interviews": 1
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2026-09-14",
         "quota_id": "AA-DFW",
+        "Number of interviews": 14
+    },
+    {
+        "InterviewDate": "2026-09-14",
+        "quota_id": "AA-DSM",
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2026-09-14",
+        "quota_id": "AA-LAX",
         "Number of interviews": 1
     },
     {
         "InterviewDate": "2026-09-14",
         "quota_id": "AA-MCI",
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2026-09-14",
+        "quota_id": "AA-MIA",
+        "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2026-09-14",
+        "quota_id": "AA-MSP",
         "Number of interviews": 1
     },
     {
         "InterviewDate": "2026-09-14",
+        "quota_id": "AA-OMA",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2026-09-14",
         "quota_id": "AA-ONT",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2026-09-14",
+        "quota_id": "AA-RNO",
         "Number of interviews": 1
     },
     {
         "InterviewDate": "2026-09-14",
         "quota_id": "AA-SFO",
-        "Number of interviews": 2
+        "Number of interviews": 12
+    },
+    {
+        "InterviewDate": "2026-09-14",
+        "quota_id": "AA-SLC",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2026-09-14",
+        "quota_id": "AA-SNA",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2026-09-14",
+        "quota_id": "AA-STL",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2026-09-14",
+        "quota_id": "AA-TUS",
+        "Number of interviews": 11
+    },
+    {
+        "InterviewDate": "2026-09-14",
+        "quota_id": "AA-XNA",
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2026-09-14",
         "quota_id": "WN-COS",
-        "Number of interviews": 1
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2026-09-14",
+        "quota_id": "WS-YYC",
+        "Number of interviews": 2
     }
 ] `;
